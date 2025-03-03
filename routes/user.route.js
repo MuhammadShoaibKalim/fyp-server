3948ca31e5bd3621d43f24a21fca45c1a76e435c
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-03 23:52:33 +0500

    Refactor user routes to integrate order management and update profile endpoints

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,7 +1,21 @@
 import express from "express";
-import { userRegister, userLogin, userLogout, getUserProfile, updateUserProfile, changePassword, deleteUserAccount } from "../controllers/user.controller.js";
+import { 
+    userRegister, 
+    userLogin, 
+    userLogout, 
+    getUserProfile, 
+    updateUserProfile, 
+    changePassword, 
+    deleteUserAccount } from "../controllers/auth.controller.js";
+import {
+    getRecommendedTests,
+    bookTestOrPackage,
+    placeOrder,
+    getUserOrders,
+    getOrderDetails,
+    cancelOrder
+} from "../controllers/order.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";  
-
 const router = express.Router();
 
 //  Public Routes
@@ -11,8 +25,17 @@ router.get("/logout", userLogout);
 
 // Protected Routes (Require Authentication)
 router.get("/profile", isAuthenticated, getUserProfile);
-router.put("/update-profile", isAuthenticated, updateUserProfile);
+router.put("/:id", isAuthenticated, updateUserProfile);
 router.put("/change-password", isAuthenticated, changePassword);
-router.delete("/delete-account", isAuthenticated, deleteUserAccount);
+router.delete("/:id", isAuthenticated, deleteUserAccount);
+
+
+// User Services Routes
+// router.post("/recommend-tests", isAuthenticated, getRecommendedTests); 
+router.post("/book-test", isAuthenticated, bookTestOrPackage); 
+router.post("/place-order", isAuthenticated, placeOrder); 
+router.get("/orders", isAuthenticated, getUserOrders);
+router.get("/order/:orderId", isAuthenticated, getOrderDetails); 
+router.delete("/order/:orderId/cancel", isAuthenticated, cancelOrder); 
 
 export default router;
