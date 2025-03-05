ea4ad963d189e53696d9e002c7878bf44eec3bb8
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-05 23:50:19 +0500

    Refactor collections routes: update authentication middleware and modify route handlers for better clarity and structure

--- routes/collections.route.js
+++ routes/collections.route.js
@@ -1,20 +1,19 @@
 import express from "express";
 import {
+  getCollections,
+  getSpecificCollectionById,
   addCollection,
   updateCollection,
   deleteCollection,
-  getCollections,
-  getCollectionById,
 } from "../controllers/collections.controller.js";
-import { isAuthenticated,isRole } from "../middlewares/auth.middleware.js";
+import { isAuthenticated, isLabAdmin, isSuperAdmin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.use(isAuthenticated, isRole(["Super Admin"])); 
-router.post("/", addCollection); 
-router.get("/", getCollections); 
-router.get("/:id", getCollectionById); 
-router.put("/:id", updateCollection); 
-router.delete("/:id", deleteCollection); 
+router.post("/add-collection", isAuthenticated, addCollection);
+router.get("/", isAuthenticated, getCollections);
+router.get("/:id", isAuthenticated, getSpecificCollectionById);
+router.put("/:id", isAuthenticated, updateCollection);
+router.delete("/:id", isAuthenticated, deleteCollection);
 
 export default router;
