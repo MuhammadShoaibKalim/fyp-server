2f080e11d0295199a0a1175faef3d93c07229c8a
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-05 23:50:10 +0500

    Add authentication and user service routes: implement registration, login, profile management, and order handling with authentication middleware

---
+++ routes/auth.route.js
@@ -0,0 +1,43 @@
+import express from "express";
+import { 
+    userRegister, 
+    userLogin, 
+    userLogout, 
+    getUserProfile, 
+    updateUserProfile, 
+    changePassword, 
+    deleteUserAccount } from "../controllers/auth.controller.js";
+import {
+    getRecommendedTests,
+    bookTestOrPackage,
+    placeOrder,
+    getUserOrders,
+    getOrderDetails,
+    cancelOrder
+} from "../controllers/order.controller.js";
+import { isAuthenticated } from "../middlewares/auth.middleware.js";  
+const router = express.Router();
+
+//  Public Routes
+router.post("/register", userRegister);
+router.post("/login", userLogin);
+router.get("/logout", userLogout);
+
+// Protected Routes (Require Authentication)
+router.get("/profile", isAuthenticated, getUserProfile);
+router.put("/:id", isAuthenticated, updateUserProfile);
+router.put("/change-password", isAuthenticated, changePassword);
+router.delete("/:id", isAuthenticated, deleteUserAccount);
+
+
+// User Services Routes
+// router.post("/recommend-tests", isAuthenticated, getRecommendedTests); 
+router.post("/book-test", isAuthenticated, bookTestOrPackage); 
+router.post("/place-order", isAuthenticated, placeOrder); 
+router.get("/orders", isAuthenticated, getUserOrders);
+router.get("/order/:orderId", isAuthenticated, getOrderDetails); 
+router.delete("/order/:orderId/cancel", isAuthenticated, cancelOrder); 
+
+
+
+export default router;
