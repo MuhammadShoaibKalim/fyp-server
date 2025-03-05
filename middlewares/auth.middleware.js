5c13a7de2ccc646ba499d1d92d3bdca970aa5324
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-05 23:46:57 +0500

    Enhance authentication middleware: update role checks for Lab Admins and Super Admins, add new protect middleware, and improve error messages.

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/auth.model.js";
 
-// Authentication Middleware
+// Middleware to Authenticate User
 export const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
@@ -11,6 +11,7 @@ export const isAuthenticated = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    console.log()
     if (!decoded || !decoded.id) {
       return res.status(401).json({ message: "Unauthorized: Invalid token" });
     }
@@ -48,8 +49,8 @@ export const isRole = (roles) => {
 // Middleware to Ensure Lab Admin Access
 export const isLabAdmin = async (req, res, next) => {
   try {
-    if (req.user.role !== "lab-admin") {
-      return res.status(403).json({ message: "Forbidden: Only Lab Admins can access this" });
+    if (req.user.role !== "Lab Admin") {
+      return res.status(403).json({ message: "Access denied. Only Lab Admins allowed." });
     }
 
     // Ensure Lab Admin can only access their assigned lab
@@ -62,5 +63,45 @@ export const isLabAdmin = async (req, res, next) => {
   } catch (error) {
     return res.status(500).json({ message: "Server Error", error: error.message });
   }
+};
+
+// Middleware to Ensure Lab Admin Access
+export const protect = async (req, res, next) => {
+  let token;
+  
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      console.log("Token:",token)
+
+      const decoded = jwt.verify(token, process.env.SUPERADMIN_SECRET_KEY);
+      console.log("decode:",decoded)
+
+      req.user = await User.findById(decoded.id).select("-password");
+      console.log("user:",req.user )
+
+      if (!req.user) {
+        return res.status(401).json({ message: "Unauthorized: User not found" });
+      }
+
+      if (req.user.role !== "Super Admin" && req.user.role !== "Lab Admin") {
+        return res.status(403).json({ message: "Forbidden: Access Denied" });
+      }
+
+      next(); 
+    } catch (error) {
+      return res.status(401).json({ message: "Unauthorized: Invalid token" });
+    }
+  } else {
+    return res.status(401).json({ message: "Unauthorized: No token provided" });
+  }
+};
+
+// Check if user is a Super Admin
+export const isSuperAdmin = (req, res, next) => {
+  if (req.user.role !== "Super Admin") {
+      return res.status(403).json({ message: "Access denied. Only Super Admins allowed." });
+  }
+  next();
 };
 
