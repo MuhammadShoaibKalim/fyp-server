3b8af8e25560bce7491a8079612d08927e6ce89a
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-05 23:51:01 +0500

    Refactor user routes: consolidate user management functionality, implement role-based access for lab admin creation, and enhance route structure for clarity

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,41 +1,18 @@
 import express from "express";
-import { 
-    userRegister, 
-    userLogin, 
-    userLogout, 
-    getUserProfile, 
-    updateUserProfile, 
-    changePassword, 
-    deleteUserAccount } from "../controllers/auth.controller.js";
+import { isAuthenticated, isRole } from "../middlewares/auth.middleware.js";
 import {
-    getRecommendedTests,
-    bookTestOrPackage,
-    placeOrder,
-    getUserOrders,
-    getOrderDetails,
-    cancelOrder
-} from "../controllers/order.controller.js";
-import { isAuthenticated } from "../middlewares/auth.middleware.js";  
+  getUsers, 
+  updateUser,
+  deleteUser,
+} from "../controllers/user.controller.js";
+import { createLabAdmin } from "../controllers/superAdmin.controller.js";
+
 const router = express.Router();
 
-//  Public Routes
-router.post("/register", userRegister);
-router.post("/login", userLogin);
-router.get("/logout", userLogout);
-
-// Protected Routes (Require Authentication)
-router.get("/profile", isAuthenticated, getUserProfile);
-router.put("/:id", isAuthenticated, updateUserProfile);
-router.put("/change-password", isAuthenticated, changePassword);
-router.delete("/:id", isAuthenticated, deleteUserAccount);
-
-
-// User Services Routes
-// router.post("/recommend-tests", isAuthenticated, getRecommendedTests); 
-router.post("/book-test", isAuthenticated, bookTestOrPackage); 
-router.post("/place-order", isAuthenticated, placeOrder); 
-router.get("/orders", isAuthenticated, getUserOrders);
-router.get("/order/:orderId", isAuthenticated, getOrderDetails); 
-router.delete("/order/:orderId/cancel", isAuthenticated, cancelOrder); 
+// Super Admin Creates Lab Admin(Users)
+router.post("/create-labadmin", isAuthenticated, isRole(["Super Admin"]), createLabAdmin);
+router.get("/", isAuthenticated, isRole(["Super Admin"]), getUsers);
+router.put("/:id", isAuthenticated, updateUser);
+router.delete("/:id", isAuthenticated, isRole(["Super Admin"]), deleteUser);
 
 export default router;
