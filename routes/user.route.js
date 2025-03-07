e18db4110a63662644975d5006d4573451e29641
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-07 23:58:06 +0500

    Refactor user routes: update access control middleware to use isSuperAdmin for lab admin management

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { isAuthenticated, isRole } from "../middlewares/auth.middleware.js";
+import { isAuthenticated, isSuperAdmin } from "../middlewares/auth.middleware.js";
 import {
   getUsers, 
   updateUser,
@@ -10,9 +10,9 @@ import { createLabAdmin } from "../controllers/superAdmin.controller.js";
 const router = express.Router();
 
 // Super Admin Creates Lab Admin(Users)
-router.post("/create-labadmin", isAuthenticated, isRole(["Super Admin"]), createLabAdmin);
-router.get("/", isAuthenticated, isRole(["Super Admin"]), getUsers);
-router.put("/:id", isAuthenticated, updateUser);
-router.delete("/:id", isAuthenticated, isRole(["Super Admin"]), deleteUser);
+router.post("/create-labadmin", isAuthenticated, isSuperAdmin, createLabAdmin);
+router.get("/", isAuthenticated, isSuperAdmin, getUsers);
+router.put("/:id", isAuthenticated,isSuperAdmin, updateUser);
+router.delete("/:id", isAuthenticated,isSuperAdmin, deleteUser);
 
 export default router;
