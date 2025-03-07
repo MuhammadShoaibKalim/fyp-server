918ad8f05863b68a0d3b2c141cd25a3d4bec611c
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-07 23:57:16 +0500

    Remove unused order service routes from authentication router

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -7,14 +7,6 @@ import {
     updateUserProfile, 
     changePassword, 
     deleteUserAccount } from "../controllers/auth.controller.js";
-import {
-    getRecommendedTests,
-    bookTestOrPackage,
-    placeOrder,
-    getUserOrders,
-    getOrderDetails,
-    cancelOrder
-} from "../controllers/order.controller.js";
 import { isAuthenticated } from "../middlewares/auth.middleware.js";  
 const router = express.Router();
 
@@ -28,15 +20,6 @@ router.get("/profile", isAuthenticated, getUserProfile);
 router.put("/:id", isAuthenticated, updateUserProfile);
 router.put("/change-password", isAuthenticated, changePassword);
 router.delete("/:id", isAuthenticated, deleteUserAccount);
-
-
-// User Services Routes
-// router.post("/recommend-tests", isAuthenticated, getRecommendedTests); 
-router.post("/book-test", isAuthenticated, bookTestOrPackage); 
-router.post("/place-order", isAuthenticated, placeOrder); 
-router.get("/orders", isAuthenticated, getUserOrders);
-router.get("/order/:orderId", isAuthenticated, getOrderDetails); 
-router.delete("/order/:orderId/cancel", isAuthenticated, cancelOrder); 
 
 
 
