98f8dad54ba6f2de3a3298dbb018a2fab12c70cf
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-07 23:57:30 +0500

    Refactor labAdmin routes: enhance access control by updating middleware and adding new endpoints for lab admin profile management

--- routes/labAdmin.route.js
+++ routes/labAdmin.route.js
@@ -1,16 +1,25 @@
-
 import express from "express";
-import {isAuthenticated, isRole} from "../middlewares/auth.middleware.js";
-
-import { 
-    loginLabAdmin,
-    logoutLabAdmin,
-    getLabAdminOverview
+import {
+  getLabAdminOverview,
+  updateLabAdminProfile,
+  getInboxMessages,
+  respondToInboxMessage,
+  getLabAdminProfile,
+  loginLabAdmin,
+  logoutLabAdmin,
+  updateLabDetails
 } from "../controllers/labAdmin.controller.js";
+import { isAuthenticated, isLabAdmin,  } from "../middlewares/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/login", loginLabAdmin);
 router.post("/logout", logoutLabAdmin);
-router.get("/overview", isAuthenticated, isRole(["Super Admin"]), getLabAdminOverview);
+router.get("/overview",isLabAdmin, isAuthenticated , getLabAdminOverview);
+router.get("/:id", isLabAdmin, getLabAdminProfile);
+router.put("/:id", isLabAdmin, updateLabAdminProfile);
+router.put("/:id", isLabAdmin, updateLabDetails);
+router.get("/inbox", isLabAdmin, getInboxMessages);
+router.post("/inbox/:id/respond", isLabAdmin, respondToInboxMessage);
 
 export default router;
