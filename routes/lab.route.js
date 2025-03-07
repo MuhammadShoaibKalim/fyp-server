2558cb265eadb8b73219cd15b8b744d3010494a0
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-07 23:57:23 +0500

    Refactor lab routes: update authentication middleware and streamline access control for lab operations

--- routes/lab.route.js
+++ routes/lab.route.js
@@ -1,26 +1,18 @@
 import express from "express";
 import {
-    addLab, getLabs, updateLab, deleteLab, addTestToLab,
-    getCollectionsByLab
+  addLab,
+  getLab,
+  updateLab,
+  deleteLab,
 } from "../controllers/lab.controller.js";
-import { protect, isSuperAdmin, isLabAdmin, isAuthenticated } from "../middlewares/auth.middleware.js";
+import { isLabAdmin, isAuthenticated } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-//  Create a Lab (Super Admin or Lab Admin)
-router.post("/create", protect,  addLab);
-
-// Get Labs (Super Admin sees all, Lab Admin sees their own)
-router.get("/", protect, getLabs);
-
-// Update Lab (Super Admin can update all, Lab Admin can update their own)
-router.put("/:id", protect, updateLab);
-
-// Delete Lab (Only Super Admin)
-router.delete("/:id", protect, isSuperAdmin, deleteLab);
-
-// fetch all tests/packages for a lab
-router.get("/:labId", isAuthenticated, getCollectionsByLab);
-
+router.post("/create", isLabAdmin, isAuthenticated, addLab);
+router.put("/:id", isLabAdmin, isAuthenticated, updateLab);
+router.get("/", isLabAdmin, isAuthenticated, getLab);
+router.post("/create", isLabAdmin, isAuthenticated, addLab);
+router.delete("/:id", isLabAdmin, isAuthenticated, deleteLab);
 
 export default router;
