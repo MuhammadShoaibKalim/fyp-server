c32a3e8fc0818fb5cb0cf8391d21292f33a3044a
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-07 23:58:13 +0500

    Enhance token generation: add error handling for missing secret key and improve expiration time

--- utils/auth.util.js
+++ utils/auth.util.js
@@ -1,10 +1,17 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (user) => {
-    return jwt.sign({ 
-        id: user._id, 
-        role: user.role 
-       },
-        process.env.SECRET_KEY, { expiresIn: "7d" }
-    );
+    if (!process.env.SECRET_KEY) {
+        throw new Error("Secret key is not defined in environment variables");
+    }
+
+    try {
+        return jwt.sign(
+            { id: user._id, role: user.role  },
+              process.env.SECRET_KEY,
+            { expiresIn: 3 * 24 * 60 * 60 }
+        );
+    } catch (error) {
+        throw new Error("Error generating token: " + error.message);
+    }
 };
