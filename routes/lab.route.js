f2cc7e8ee38adcfde6e02931652fca243f90cef1
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-11 23:50:33 +0500

    Refactor lab routes: update authentication middleware for lab retrieval and deletion; streamline route definitions

--- routes/lab.route.js
+++ routes/lab.route.js
@@ -1,17 +1,20 @@
 import express from "express";
 import {
   addLab,
-  getLab,
   updateLab,
   deleteLab,
+  // getLabById,
+  // getAllLabs,
+  getLab,
 } from "../controllers/lab.controller.js";
 import {  isAuthenticated, isLabAdmin, isSuperAdmin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/create", isAuthenticated, isSuperAdmin , addLab);
+router.get("/", isAuthenticated, isLabAdmin, getLab);
+// router.get("/:id", isAuthenticated, isSuperAdmin, isLabAdmin, getLabById);
 router.put("/:id", isAuthenticated, isLabAdmin, updateLab);
-router.get("/", isAuthenticated, isSuperAdmin, isLabAdmin, getLab);
-router.delete("/:id", isLabAdmin, isAuthenticated, deleteLab);
+router.delete("/:id", isAuthenticated,isLabAdmin, deleteLab);
 
 export default router;
