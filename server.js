32b00196d0868e812e43d41affab4bfc56610be0
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2025-03-17 06:03:34 +0500

    Add payment routes: integrate paymentRoutes for handling payment-related requests

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import userRoutes from "./routes/user.route.js";
 import queryRoutes from "./routes/query.route.js"
 import aiRoutes from "./routes/ai.route.js"
 import cartRoutes from "./routes/cart.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 import testPackageRoutes from "./routes/testpackage.route.js"
 import cors from "cors";
 
@@ -26,6 +27,7 @@ const app = express();
 //   origin: "http://localhost:5173", 
 //   credentials: true,
 // }));
+app.use(cors());
 
 // Middleware
 app.use(express.json());
@@ -44,6 +46,8 @@ app.use("/api/query", queryRoutes);
 app.use("/api/ai", aiRoutes)
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/payment", paymentRoutes);
+
 
 
 // Connect to MongoDB
